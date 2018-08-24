f783aa8ff9e409455e96089e0ea971dd72d6780b
Christophe Willemsen <willemsen.christophe@gmail.com>
2018-08-24 17:56:37 +0200

    add missing annotations

--- src/main/java/org/ikwattro/sandbox/SecurityConfiguration.java
+++ src/main/java/org/ikwattro/sandbox/SecurityConfiguration.java
@@ -1,8 +1,12 @@
 package org.ikwattro.sandbox;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
